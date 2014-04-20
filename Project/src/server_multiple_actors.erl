e22707c9adf8fa211ed7f79b50752be05a8108c4
overhaeg <overhaeg@vub.ac.be>
2014-04-20 04:20:45 +0200

    Added tweet and get_tweet functionality

--- Project/src/server_multiple_actors.erl
+++ Project/src/server_multiple_actors.erl
@@ -32,6 +32,11 @@ subscribe(EntryPid, UserId, UserIdToSubscribeTo) ->
         {EntryPid, subscribed, UserId, UserIdToSubscribeTo} -> ok
     end.
 
+%%
+%% Actors
+%%
+
+% 
 manager_actor(Users) ->
     receive
         
@@ -51,6 +56,7 @@ manager_actor(Users) ->
 data_actor(Data) ->
     io:format("Data Actor creation ~n"),
     receive
+
         {Sender, get_data} ->
             Sender ! {self(), data, Data},
             data_actor(Data);
@@ -60,11 +66,26 @@ data_actor(Data) ->
             Sender ! {self(), followed_by_user, UserId, UserIdFollowing},
             data_actor(NewData);
         
+        {Sender, get_tweets, UserId, Page} ->
+            Sender ! {self(), tweets, UserId, Page, tweets(Data, Page)},
+            data_actor(Data);
+
+        {Sender, tweet, UserId, Tweet} -> 
+            {NewData, Timestamp} = tweet(Data, UserId, Tweet),
+            Sender ! {self(), tweet_accepted, UserId, Timestamp},
+            data_actor(NewData);
+
+        {Sender, tweet_to_follower, UserId, Timestamp, Tweet} ->
+            NewData = subscribed_tweet(Data, UserId, Timestamp, Tweet),
+            data_actor(NewData);
+
+
         {Sender, subscribe, UserId, UserIdToSubscribeTo} ->
             io:format("subscribe ~n"),
             NewData = subscribe_to_user(Data, UserId, UserIdToSubscribeTo),
             Sender ! {self(), subscribed, UserId, UserIdToSubscribeTo},
             data_actor(NewData)
+
             
     end.
 
@@ -94,21 +115,25 @@ entry_actor() ->
     end,
     entry_actor().
 
+%%
+%% Internal Functions
+%%
+
 get_data_actor(Users, UserId) ->
     {user, UserId, DataActor} = lists:nth(UserId + 1, Users),
     DataActor.
 
 add_new_user(Users) ->
     NewUserId = length(Users),
-    NewDataActor = spawn_link(?MODULE, data_actor, [{data, [], sets:new(), sets:new()}]), %% data, tweets, subscriptions, followed by
+    NewDataActor = spawn_link(?MODULE, data_actor, [{data, [], [], sets:new(), sets:new()}]), %% data, tweets, subscribedTweets, subscriptions, followed by
     NewUsers = Users ++ [{user, NewUserId, NewDataActor}],
     NewUserActor = spawn_link(?MODULE, entry_actor, []),
     {NewUsers, NewUserId, NewUserActor}.
 
 subscribe_to_user(Data, UserId, UserIdToSubscribeTo) ->
     io:format("Was Here~n"),
-    {data, Tweets, Subscriptions, Followed_By} = Data,
-    NewData = {data, Tweets, sets:add_element(UserIdToSubscribeTo, Subscriptions), Followed_By},
+    {data, Tweets, SubscribedTweets, Subscriptions, Followed_By} = Data,
+    NewData = {data, Tweets, SubscribedTweets, sets:add_element(UserIdToSubscribeTo, Subscriptions), Followed_By},
     ManagerActor = whereis(manager_actor),
     ManagerActor ! {self(), get_data_actor, UserIdToSubscribeTo},
     io:format("Manager_actor_get 2 ~n"),
@@ -123,9 +148,44 @@ subscribe_to_user(Data, UserId, UserIdToSubscribeTo) ->
 
 follow_user(Data, UserIdFollowing) ->
     io:format("followed ~n"),
-    {data, Tweets, Subscriptions, Followed_By} = Data,
-    NewData = {data, Tweets, Subscriptions, sets:add_element(UserIdFollowing, Followed_By)},
+    {data, Tweets, SubscribedTweets, Subscriptions, Followed_By} = Data,
+    NewData = {data, Tweets, SubscribedTweets, Subscriptions, sets:add_element(UserIdFollowing, Followed_By)},
     NewData.
+
+tweets(Data, _Page) ->
+    {data, Tweets, _SubscribedTweets, _Subscriptions, _Followed_By} = Data,
+    Tweets.
+
+tweet(Data, UserId, Tweet) ->
+    io:format("tweeting ~n"),
+    {data, Tweets, SubscribedTweets, Subscriptions, Followed_By} = Data,
+    Timestamp = erlang:now(),
+    NewData = {data, Tweets ++ [{tweet, UserId, Timestamp, Tweet}], SubscribedTweets, Subscriptions, Followed_By},
+    tweet_to_followers(sets:to_list(Followed_By), Tweet, Timestamp, UserId),
+    {NewData, Timestamp}.
+
+tweet_to_followers(Followers, Tweet, Timestamp, UserId) ->
+    if
+        Followers /= [] ->
+            Follower = lists:nth(1, Followers),
+            ManagerActor = whereis(manager_actor),
+            ManagerActor ! {self(), get_data_actor, Follower},
+            receive
+                {data_actor, DataActor} ->
+                    DataActor ! {self(), tweet_to_follower, UserId, Timestamp, Tweet},
+                    tweet_to_followers(lists:nthtail(1, Followers), Tweet, Timestamp, UserId)
+            end;
+        true -> ok
+    end.
+
+subscribed_tweet(Data, UserId, Timestamp, Tweet) ->
+    {data, Tweets, SubscribedTweets, Subscriptions, Followed_By} = Data,
+    NewData = {data, Tweets, SubscribedTweets ++ [{tweet, UserId, Timestamp, Tweet}], Subscriptions, Followed_By},
+    NewData.
+
+            
+
+%% Tests
 
 initialization_test() ->
     catch unregister(manager_actor),

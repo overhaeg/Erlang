c6d1c4ea197719722422b9c1739a39d306692851
overhaeg <overhaeg@vub.ac.be>
2014-04-20 08:25:20 +0200

    should be finished

--- Project/src/server_single_actor.erl
+++ Project/src/server_single_actor.erl
@@ -1,154 +1,199 @@
-%% This is a simple single-actor example implementation of the project.
-%%
-%% It will create an actor per client connection, and one actor to represent
-%% all internal state (all users, their subscriptions, and their tweets).
-%%
-%% This implementation is provided with unit tests. However, these tests are
-%% neither complete nor implementation independent. Thus, be careful when
-%% reusing them.
--module(server_single_actor).
+-module(server_multiple_actors).
+
+-export([initialize/0,
+         register_user/0,
+         subscribe/3,
+
+         manager_actor/1,
+         data_actor/1,
+         entry_actor/0]).
 
 %%
 %% Include files
 %%
 -include_lib("eunit/include/eunit.hrl").
 
-%%
-%% Exported Functions
-%%
--export([initialize/0,
-         register_user/0,
-         subscribe/3,
-
-         % internal actors
-         data_actor/1,
-         entry_actor/0]).
-
-%%
-%% API Functions
-%%
-
-% Start server.
 initialize() ->
-    register(data_actor, spawn_link(?MODULE, data_actor, [[]])),
+    io:format("Initialize"),
+    register(manager_actor, spawn_link(?MODULE, manager_actor, [[]])),
     ok.
 
-% Register a new user and return its id and the entry actor that is
-% responsible for this user.
--spec register_user() -> {integer(), pid()}.
+
 register_user() ->
-    data_actor ! {self(), register_user},
+    manager_actor ! {self(), register_user},
     receive
         {registered_user, UserId, EntryPid} -> {UserId, EntryPid}
     end.
 
-% Subscribe/follow another user.
--spec subscribe(pid(), integer(), integer()) -> ok.
 subscribe(EntryPid, UserId, UserIdToSubscribeTo) ->
+    io:format("Initial subscribe call~n"),
     EntryPid ! {self(), subscribe, UserId, UserIdToSubscribeTo},
     receive
         {EntryPid, subscribed, UserId, UserIdToSubscribeTo} -> ok
     end.
 
-% The data actor works like a small database and encapsulates all state of this
-% simple implementation.
-data_actor(Data) ->
+%%
+%% Actors
+%%
+
+% 
+manager_actor(Users) ->
     receive
-        {Sender, register_user} ->
-            {NewData, NewUserId, NewUserActor} = add_new_user(Data),
-            Sender ! {registered_user, NewUserId, NewUserActor},
-            data_actor(NewData);
-            
-        {Sender, get_timeline, UserId, Page} ->
-            Sender ! {self(), timeline, UserId, Page, timeline(Data, UserId, Page)},
-            data_actor(Data);
-            
-        {Sender, get_tweets,   UserId, Page} ->
-            Sender ! {self(), tweets,   UserId, Page, tweets(Data, UserId, Page)},
-            data_actor(Data);
         
-        {Sender, tweet,        UserId, Tweet} ->
+        {Sender, register_user} ->
+            {NewUsers, NewUserID, NewUserActor} = add_new_user(Users),
+            Sender ! {registered_user, NewUserID, NewUserActor},
+            manager_actor(NewUsers);
+       
+        {Sender, get_data_actor, UserId} ->
+            io:format("get_data_actor ~n"),
+            Sender ! {data_actor, get_data_actor(Users, UserId)},
+            manager_actor(Users)
+
+        end.
+
+
+data_actor(Data) ->
+    io:format("Data Actor creation ~n"),
+    receive
+
+
+        {Sender, follow_user, UserId, UserIdFollowing} ->
+            NewData = follow_user(Data, UserIdFollowing),
+            Sender ! {self(), followed_by_user, UserId, UserIdFollowing},
+            data_actor(NewData);
+        
+        {Sender, get_tweets, UserId, Page} ->
+            Sender ! {self(), tweets, UserId, Page, tweets(Data, Page)},
+            data_actor(Data);
+
+        {Sender, tweet, UserId, Tweet} -> 
             {NewData, Timestamp} = tweet(Data, UserId, Tweet),
             Sender ! {self(), tweet_accepted, UserId, Timestamp},
             data_actor(NewData);
+
+        {Sender, tweet_to_follower, UserId, Timestamp, Tweet} ->
+            NewData = subscribed_tweet(Data, UserId, Timestamp, Tweet),
+            data_actor(NewData);
         
-        {Sender, subscribe,    UserId, UserIdToSubscribeTo} ->
+        {Sender, get_timeline, UserId, Page} ->
+            Sender ! {self(), timeline, UserId, Page, timeline(Data, Page)},
+            data_actor(Data);
+
+        {Sender, subscribe, UserId, UserIdToSubscribeTo} ->
+            io:format("subscribe ~n"),
             NewData = subscribe_to_user(Data, UserId, UserIdToSubscribeTo),
             Sender ! {self(), subscribed, UserId, UserIdToSubscribeTo},
             data_actor(NewData)
+
+            
     end.
 
-% This simple implementation of the entry actor just delegates to the data
-% actor.
+
 entry_actor() ->
-    DataActor = whereis(data_actor),
+    io:format("Entry actor~n"),
+    ManagerActor = whereis(manager_actor),
     receive
         % RequestType ::= tweets | timeline
-        {Sender, RequestType, UserId, PageOrTweetOrUserId} -> 
-            DataActor ! {self(), RequestType, UserId, PageOrTweetOrUserId},
-            
+        {Sender, RequestType, UserId, PageOrTweetOrUserId} ->
+            ManagerActor ! {self(), get_data_actor, UserId},
+            io:format("After Manager Actor~n"),
             receive
-                {DataActor, ResponseType, UserId, Page, Result} ->
-                    Sender ! {self(), ResponseType, UserId, Page, Result};
-                {DataActor, tweet_accepted, UserId, Timestamp} ->
-                    Sender ! {self(), tweet_accepted, UserId, Timestamp};
-                {DataActor, subscribed, UserId, PageOrTweetOrUserId} ->
-                    Sender ! {self(), subscribed, UserId, PageOrTweetOrUserId}
+                {data_actor, DataActor} ->
+                    io:format("Data Actor ID"),
+                    DataActor ! {self(), RequestType, UserId, PageOrTweetOrUserId},
+                    receive
+                        {DataActor, ResponseType, UserId, Page, Result} ->
+                            Sender ! {self(), ResponseType, UserId, Page, Result};
+                        {DataActor, tweet_accepted, UserId, Timestamp} ->
+                            Sender ! {self(), tweet_accepted, UserId, Timestamp};
+                        {DataActor, subscribed, UserId, PageOrTweetOrUserId} ->
+                            Sender ! {self(), subscribed, UserId, PageOrTweetOrUserId}
+                    end
             end
+
     end,
     entry_actor().
 
 %%
 %% Internal Functions
 %%
-add_new_user(Data) ->
-    NewUserId = length(Data),
-    NewData = Data ++ [{user, NewUserId, [], sets:new()}],
+
+get_data_actor(Users, UserId) ->
+    {user, UserId, DataActor} = lists:nth(UserId + 1, Users),
+    DataActor.
+
+add_new_user(Users) ->
+    NewUserId = length(Users),
+    NewDataActor = spawn_link(?MODULE, data_actor, [{data, [], [], sets:new(), sets:new()}]), %% data, tweets, subscribedTweets, subscriptions, followed by
+    NewUsers = Users ++ [{user, NewUserId, NewDataActor}],
     NewUserActor = spawn_link(?MODULE, entry_actor, []),
-    {NewData, NewUserId, NewUserActor}.
-
-timeline(Data, UserId, _Page) ->
-    {user, UserId, Tweets, Subscriptions} = lists:nth(UserId + 1, Data),
-    
-    UnsortedTweetsForTimeLine =
-        lists:foldl(fun(UserId2, AllTweets) ->
-                        {_, _, SomeTweets, _} = lists:nth(UserId2 + 1, Data),
-                        AllTweets ++ SomeTweets
-                    end,
-                    Tweets,
-                    sets:to_list(Subscriptions)),
-    SortedTweets = lists:reverse(lists:keysort(3, UnsortedTweetsForTimeLine)),
-    lists:sublist(SortedTweets, 10).
-
-tweets(Data, UserId, _Page) ->
-    {user, UserId, Tweets, _Subscriptions} = lists:nth(UserId + 1, Data),
-    Tweets.
-
-tweet(Data, UserId, Tweet) ->
-    {user, UserId, Tweets, Subscriptions} = lists:nth(UserId + 1, Data),
-    Timestamp = erlang:now(),
-    NewUser = {user, UserId, Tweets ++ [{tweet, UserId, Timestamp, Tweet}], Subscriptions},
-
-    {UsersBefore, [_|UsersAfter]} = lists:split(UserId, Data),
-    {lists:append([UsersBefore, [NewUser | UsersAfter]]), Timestamp}.
+    {NewUsers, NewUserId, NewUserActor}.
 
 subscribe_to_user(Data, UserId, UserIdToSubscribeTo) ->
-    {user, UserId, Tweets, Subscriptions} = lists:nth(UserId + 1, Data),
-    NewUser = {user, UserId, Tweets, sets:add_element(UserIdToSubscribeTo, Subscriptions)},
+    io:format("Was Here~n"),
+    {data, Tweets, SubscribedTweets, Subscriptions, Followed_By} = Data,
+    NewData = {data, Tweets, SubscribedTweets, sets:add_element(UserIdToSubscribeTo, Subscriptions), Followed_By},
+    ManagerActor = whereis(manager_actor),
+    ManagerActor ! {self(), get_data_actor, UserIdToSubscribeTo},
+    io:format("Manager_actor_get 2 ~n"),
+    receive
+        {data_actor, DataActor} ->
+            io:format("sub_rec_data_actor ~n"),
+            DataActor ! {self(), follow_user, UserIdToSubscribeTo, UserId},
+            receive
+                {DataActor, followed_by_user, UserIdToSubscribeTo, UserId} -> NewData
+            end
+    end.
 
-    {UsersBefore, [_|UsersAfter]} = lists:split(UserId, Data),
-    lists:append([UsersBefore, [NewUser | UsersAfter]]).
-    
+follow_user(Data, UserIdFollowing) ->
+    io:format("followed ~n"),
+    {data, Tweets, SubscribedTweets, Subscriptions, Followed_By} = Data,
+    NewData = {data, Tweets, SubscribedTweets, Subscriptions, sets:add_element(UserIdFollowing, Followed_By)},
+    NewData.
 
-%%
-%% Test Functions
-%% 
-%% These tests are for this specific implementation. They are a partial
-%% definition of the semantics of the provided interface but also make certain
-%% assumptions of its implementation. Thus, they need to be reused with care.
-%%
+tweets(Data, _Page) ->
+    {data, Tweets, _SubscribedTweets, _Subscriptions, _Followed_By} = Data,
+    Tweets.
+
+timeline(Data, _Page) ->
+    {data, Tweets, SubscribedTweets, _Subscriptions, _Followed_By} = Data,
+    SortedTweets = lists:reverse(lists:keysort(3, Tweets ++ SubscribedTweets)),
+    SortedTweets.
+
+tweet(Data, UserId, Tweet) ->
+    io:format("tweeting ~n"),
+    {data, Tweets, SubscribedTweets, Subscriptions, Followed_By} = Data,
+    Timestamp = erlang:now(),
+    NewData = {data, Tweets ++ [{tweet, UserId, Timestamp, Tweet}], SubscribedTweets, Subscriptions, Followed_By},
+    tweet_to_followers(sets:to_list(Followed_By), Tweet, Timestamp, UserId),
+    {NewData, Timestamp}.
+
+tweet_to_followers(Followers, Tweet, Timestamp, UserId) ->
+    if
+        Followers /= [] ->
+            Follower = lists:nth(1, Followers),
+            ManagerActor = whereis(manager_actor),
+            ManagerActor ! {self(), get_data_actor, Follower},
+            receive
+                {data_actor, DataActor} ->
+                    DataActor ! {self(), tweet_to_follower, UserId, Timestamp, Tweet},
+                    tweet_to_followers(lists:nthtail(1, Followers), Tweet, Timestamp, UserId)
+            end;
+        true -> ok
+    end.
+
+subscribed_tweet(Data, UserId, Timestamp, Tweet) ->
+    {data, Tweets, SubscribedTweets, Subscriptions, Followed_By} = Data,
+    NewData = {data, Tweets, SubscribedTweets ++ [{tweet, UserId, Timestamp, Tweet}], Subscriptions, Followed_By},
+    NewData.
+
+            
+
+%% Tests
+
 initialization_test() ->
-    catch unregister(data_actor),
+    catch unregister(manager_actor),
     ?assertMatch(ok, initialize()).
 
 register_user_test() ->
@@ -162,7 +207,7 @@ register_user_test() ->
     ?assertMatch({3, _Pid4}, register_user()).
 
 init_for_test() ->
-    catch unregister(data_actor),
+    catch unregister(manager_actor),
     initialize(),
     {0, Pid1} = register_user(),
     {1, Pid2} = register_user(),
@@ -170,46 +215,8 @@ init_for_test() ->
     {3, Pid4} = register_user(),
     [Pid1, Pid2, Pid3, Pid4].
 
-timeline_test() ->
-    Pids = init_for_test(),
-    [Pid1, Pid2 | _ ] = Pids,
+subscribe_to_test() ->
+    io:format("Test"),
+    [Pid1, Pid2, _, _] = init_for_test(),
+    ?assertMatch(ok, subscribe(Pid1, 2, 1)).
 
-    ?assertMatch([], server:get_timeline(Pid1, 1, 0)),
-    ?assertMatch([], server:get_timeline(Pid2, 2, 0)).
-
-users_tweets_test() ->
-    Pids = init_for_test(),
-    [Pid1 | _ ] = Pids,
-
-    ?assertMatch([], server:get_tweets(Pid1, 1, 0)),
-    ?assertMatch([], server:get_tweets(Pid1, 2, 0)).
-
-tweet_test() ->
-    Pids = init_for_test(),
-    [Pid1, Pid2 | _ ] = Pids,
-
-    ?assertMatch([], server:get_timeline(Pid1, 1, 0)),
-    ?assertMatch([], server:get_timeline(Pid2, 2, 0)),
-
-    ?assertMatch({_MegaSecs, _Secs, _MicroSecs}, server:tweet(Pid1, 1, "Tweet no. 1")),
-
-    ?assertMatch([{tweet, _, _, "Tweet no. 1"}], server:get_tweets(Pid1, 1, 0)),
-    ?assertMatch([], server:get_tweets(Pid1, 2, 0)),
-
-    ?assertMatch([{tweet, _, _, "Tweet no. 1"}], server:get_timeline(Pid1, 1, 0)), % own tweets included in timeline
-    ?assertMatch([], server:get_timeline(Pid2, 2, 0)),
-
-    Pids. % no subscription
-
-subscription_test() ->
-    [_Pid1, Pid2 | _ ] = tweet_test(),
-
-    ?assertMatch(ok, subscribe(Pid2, 2, 1)),
-
-    ?assertMatch([{tweet, _, _, "Tweet no. 1"}], server:get_timeline(Pid2, 2, 0)), % now there is a subscription
-
-    ?assertMatch({_MegaSecs, _Secs, _MicroSecs}, server:tweet(Pid2, 2, "Tweet no. 2")),
-    ?assertMatch([{tweet, _, _, "Tweet no. 2"},
-                  {tweet, _, _, "Tweet no. 1"}],
-                 server:get_timeline(Pid2, 2, 0)),
-    done. 
